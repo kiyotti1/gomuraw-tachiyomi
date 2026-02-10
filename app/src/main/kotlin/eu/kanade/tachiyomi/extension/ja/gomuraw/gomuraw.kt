a5e563044b1f2f4338da8b88ae9303b51ba0fe41
kiyotti1 <kiyottikiyora@gmail.com>
2026-02-10 18:52:31 +0900

    Update gomuraw.kt

--- app/src/main/kotlin/eu/kanade/tachiyomi/extension/ja/gomuraw/gomuraw.kt
+++ app/src/main/kotlin/eu/kanade/tachiyomi/extension/ja/gomuraw/gomuraw.kt
@@ -1,23 +1,16 @@
 package eu.kanade.tachiyomi.extension.ja.gomuraw
 
-import eu.kanade.tachiyomi.source.model.*
 import okhttp3.Request
+// インポートを削除（Stub内に定義したため不要）
 
 class gomuraw : MangaReader("gomuraw", "https://gomuraw.dev", "ja") {
     
-    // 変数名を MangaReader の定義に合わせて修正
     override val popularMangaSelector = "div.bs, div.manga-item, div.utao, div.listupd div.bs"
-    
     override val popularMangaNextPageSelector = "div.pagination a.next, .next.page-numbers"
-
-    // 検索設定
     override val searchPathSegment = "search"
     override val searchKeyword = "q"
-
-    // チャプターリストの設定
     override val chapterIdSelect = "ja-chaps"
 
-    // 画像URL取得用の設定
     override fun getAjaxUrl(id: String): String {
         return "$baseUrl/json/chapter?mode=vertical&id=$id"
     }
@@ -30,7 +23,7 @@ class gomuraw : MangaReader("gomuraw", "https://gomuraw.dev", "ja") {
 
     override fun getFilterList(): FilterList {
         return FilterList(
-            Note,
+            Note(),          // () を追加
             TypeFilter(),
             StatusFilter(),
             LanguageFilter(),
