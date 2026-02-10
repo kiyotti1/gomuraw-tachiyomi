2433a1ddfe8909e8a31ed3a66ef949467a9b0158
kiyotti1 <kiyottikiyora@gmail.com>
2026-02-11 08:55:20 +0900

    Update Stub.kt

--- app/src/main/kotlin/eu/kanade/tachiyomi/extension/ja/gomuraw/Stub.kt
+++ app/src/main/kotlin/eu/kanade/tachiyomi/extension/ja/gomuraw/Stub.kt
@@ -1,8 +1,7 @@
-package eu.kanade.tachiyomi.extension.ja.gomuraw
+package eu.kanade.tachiyomi.extension.ja.mangamura
 
 import okhttp3.Request
 
-// ダミーのモデルクラスを定義してインポートエラーを防ぐ
 open class MangaReader(val name: String, val baseUrl: String, val lang: String) {
     open val popularMangaSelector: String = ""
     open val popularMangaNextPageSelector: String? = null
@@ -15,7 +14,6 @@ open class MangaReader(val name: String, val baseUrl: String, val lang: String) 
     open fun getFilterList(): FilterList = FilterList()
 }
 
-// 必要なダミークラス群
 class FilterList(vararg filters: Any)
 open class Filter(val name: String)
 class Note : Filter("Note")
