a11431bf30cca50bb3bf13d90cd76fba4ced9ae2
kiyotti1 <kiyottikiyora@gmail.com>
2026-02-10 14:48:23 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        // ↓ この1行を追加
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.22")
     }
 }
@@ -15,6 +14,9 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        // ↓ この行を追加してください（Tachiyomiの部品置き場です）
+        maven { url = uri("https://raw.githubusercontent.com/tachiyomidb/maven/master") }
         maven { url = uri("https://jitpack.io") }
     }
 }
+
